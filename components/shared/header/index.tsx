57c86cb0ca775330aa8a42fccb21a9617425fc8a
Sardius <0xSardius@gmail.com>
2025-11-03 15:33:22 -0500

    added header and footer

---
+++ components/shared/header/index.tsx
@@ -0,0 +1,40 @@
+import { ShoppingCart, UserIcon } from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { APP_NAME } from "@/lib/constants";
+
+const Header = () => {
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex-between">
+        <Link href="/" className="flex-start">
+          <Image
+            src="/images/logo.svg"
+            alt={APP_NAME}
+            width={100}
+            height={100}
+            priority={true}
+          />
+          <span className="hidden font-bold text-2xl ml-3 lg:block">
+            {APP_NAME}
+          </span>
+        </Link>
+      </div>
+      <div className="space-x-2">
+        <Button asChild variant="ghost">
+          <Link href="/cart">
+            <ShoppingCart /> Cart
+          </Link>
+        </Button>
+        <Button asChild variant="ghost">
+          <Link href="/sign-in">
+            <UserIcon /> Sign In
+          </Link>
+        </Button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
